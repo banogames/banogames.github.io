e84c3e8bb3ce061287f04d7da44d992bcab1009f
banogames <63592950+banogames@users.noreply.github.com>
2022-08-24 14:26:27 +0700

    go to banoga.com

--- wp-content/themes/flatsome/inc/option.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$flatsome_tag = 'flatsome_wup_purchase_code'; 
-if ( !function_exists( 'get_lsAPI' ) )
-@include( __DIR__ . '/admin/options/update.php' ); 
-if (defined('ABSPATH')) {update_option($flatsome_tag, '846b8d75-85b3-4b3e-976c-2e29d386339d'); update_option('flatsome_wup_supported_until', '01.01.2050'); update_option('flatsome_wup_buyer', 'Licensed'); 
-update_option('flatsome_wup_sold_at', time()); delete_option('flatsome_wup_errors');}
-
